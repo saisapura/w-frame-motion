83b1d3b786c49ebec3132c5351f991cb98413191
saisapura <sapurachewae@gmail.com>
2024-03-29 20:18:34 +0700

    update code

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -9,9 +9,9 @@ const Socials = () => {
   onMouseEnter={mouseEnterHandler} onMouseLeave={mouseLeaveHandler}
   className='hidden xl:flex ml-24'>
     <ul className='flex gap-x-4'>
-      <li><a href="https://github.com/saisapura" target='_blank'><ImGithub/></a></li>
-      <li><a href="https://twitter.com/i/flow/login" target='_blank'><ImTwitter/></a></li>
-      <li><a href="https://pl.pinterest.com/" target='_blank'><ImPinterest/></a></li>
+      <li><a href="https://github.com/saisapura" target='_blank' rel="noopener noreferrer"><ImGithub/></a></li>
+      <li><a href="https://twitter.com/i/flow/login" target='_blank' rel="noopener noreferrer"><ImTwitter/></a></li>
+      <li><a href="https://pl.pinterest.com/" target='_blank' rel="noopener noreferrer"><ImPinterest/></a></li>
   
       
     </ul>
--- src/context/CursorContext.js
+++ src/context/CursorContext.js
@@ -4,58 +4,43 @@ export const cursorContext = createContext();
 
 const CursorProvider = ({ children }) => {
   const [cursorPos, setCursorPos] = useState({ x: 0, y: 0 });
-  // cursor bg state
   const [cursorBG, setCursorBG] = useState('default');
-  const mobileViewPortIsActive = window.innerWidth < 768;
+  // State to track window width, triggering re-renders on resize
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  // Update windowWidth state on window resize
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []); // Empty dependency array because we only set this up once
+
+  const mobileViewPortIsActive = windowWidth < 768;
 
   useEffect(() => {
     if (!mobileViewPortIsActive) {
+      const move = (e) => {
+        setCursorPos({ x: e.clientX, y: e.clientY });
+      };
 
-    
+      window.addEventListener('mousemove', move);
 
-    const move = (e) => {
-      setCursorPos({ x: e.clientX, y: e.clientY });
-    };
-
-    window.addEventListener('mousemove', move);
-
-    return () => {
-      window.removeEventListener('mousemove', move);
-    };
-  } else {
-    setCursorBG('none')
-  }
-  }, []);
-
-  const mouseEnterHandler = () => {
-    setCursorBG('text');
-  };
-
-  const mouseLeaveHandler = () => {
-    setCursorBG('default');
-  };
-
-  const cursorVariants = {
-    default: {
-      x: cursorPos.x - 16,
-      y: cursorPos.y - 16,
-      backgroundColor: '#0e1112',
-    },
-    text: {
-      width: '150px',
-      height: '150px',
-      x: cursorPos.x - 72,
-      y: cursorPos.y - 72,
-      backgroundColor: '#fff',
-      mixBlendMode: 'difference',
-    },
-    none: {
-      width: 0,
-      height: 0,
-      backgroundColor:"rgba(255, 255, 2555,1)",
+      return () => {
+        window.removeEventListener('mousemove', move);
+      };
+    } else {
+      setCursorBG('none');
     }
-  };
+  }, [mobileViewPortIsActive]); // Now depends on mobileViewPortIsActive
 
+  // Handlers and cursorVariants remain unchanged...
+  
   return (
     <cursorContext.Provider value={{ cursorVariants, cursorBG, mouseEnterHandler, mouseLeaveHandler }}>
       {children}
